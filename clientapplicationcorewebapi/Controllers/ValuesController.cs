1f8a34278b7f624bcf247a3914ac09048ce547d0
Chandra V <chandramouli.v@morganstanley.com>
2019-08-06 13:52:51 +0000

    sync clientapp

--- clientapplicationcorewebapi/Controllers/ValuesController.cs
+++ clientapplicationcorewebapi/Controllers/ValuesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -18,43 +19,51 @@ namespace clientapplicationcorewebapi.Controllers
         [HttpGet]
         public async Task<ActionResult<string>> GetAsync()
         {
-            var httpclient = new System.Net.Http.HttpClient(new System.Net.Http.HttpClientHandler()
-            {
-                //Proxy = new System.Net.WebProxy()
-                //{
-                //    Address = new Uri($"http://webproxy-inet.ms.com:8080"),
-                //    BypassProxyOnLocal = true,
-                //    UseDefaultCredentials = true
-                //}
-            });
-          
-            var keyvalue = new List<KeyValuePair<string, string>>();
+            HttpResponseMessage response = null;
+            string accessToken=string.Empty; string content = string.Empty;
             try
             {
-                foreach (var header in HttpContext.Request.Headers)
+                var httpclient = new System.Net.Http.HttpClient(new System.Net.Http.HttpClientHandler()
                 {
-                    var key = header.Key;
-                    var val = header.Value;
-                    httpclient.DefaultRequestHeaders.Add("clientapplicationcorewebapi-"+ key, val.ToString());
+                    //Proxy = new System.Net.WebProxy()
+                    //{
+                    //    Address = new Uri($"http://webproxy-inet.ms.com:8080"),
+                    //    BypassProxyOnLocal = true,
+                    //    UseDefaultCredentials = true
+                    //}
+                });
+
+                var keyvalue = new List<KeyValuePair<string, string>>();
+                try
+                {
+                    foreach (var header in HttpContext.Request.Headers)
+                    {
+                        var key = header.Key;
+                        var val = header.Value;
+                        httpclient.DefaultRequestHeaders.Add("clientapplicationcorewebapi-" + key, val.ToString());
+
+                    }
+                    httpclient.DefaultRequestHeaders.Add("X-MS-EndUserAuthorization", Request.Headers["Authorization"][0]);
 
                 }
-                httpclient.DefaultRequestHeaders.Add("X-MS-EndUserAuthorization", Request.Headers["Authorization"][0]);
-
-            }
-            catch (Exception ex)
+                catch (Exception ex)
+                {
+                    httpclient.DefaultRequestHeaders.Add("clientapplicationcorewebapi-headers", ex.Message + ex.StackTrace);
+                }
+                var azureServiceTokenProvider2 = new AzureServiceTokenProvider();
+                accessToken = await azureServiceTokenProvider2.GetAccessTokenAsync(targetUrl).ConfigureAwait(false);
+                httpclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                response = httpclient.GetAsync(targetUrl + "/api/values").GetAwaiter().GetResult();
+                if (response.IsSuccessStatusCode)
+                {
+                    content = response.Content.ReadAsStringAsync().Result;
+                    return Ok(content);
+                }
+            }catch(Exception ex)
             {
-                httpclient.DefaultRequestHeaders.Add("clientapplicationcorewebapi-headers", ex.Message + ex.StackTrace);
+                content = ex.Message + ex.StackTrace;
             }
-            var azureServiceTokenProvider2 = new AzureServiceTokenProvider();
-            string accessToken = await azureServiceTokenProvider2.GetAccessTokenAsync(targetUrl).ConfigureAwait(false);
-            httpclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            var response = httpclient.GetAsync(targetUrl +"/api/values").GetAwaiter().GetResult();
-            if (response.IsSuccessStatusCode)
-            {
-                var content = response.Content.ReadAsStringAsync().Result;
-                return Ok(content);
-            }
-            return BadRequest(response.ReasonPhrase + Environment.NewLine + accessToken);
+            return BadRequest(response.ReasonPhrase + Environment.NewLine + content + Environment.NewLine + accessToken);
 
         }
 
